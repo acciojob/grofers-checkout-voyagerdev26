da897b7a957affc9e9abdf862ce3a0d398902ed7
Dev Sharma <103065378+voyagerdev26@users.noreply.github.com>
2026-01-29 08:34:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,18 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+	let td=document.querySelectorAll(".price");
+	let totalPrice=0;
+	for(let t of td){
+		totalPrice+=+t.innerText;
+	}
+	let tr=document.createElement("tr");
+	let t1=document.createElement("td");
+	let t2=document.createElement("td");
+	t1.innerText="TotalPrice";
+	t2.innerText=totalPrice;
+	tr.append(t1,t2);
+	document.body.querySelector("table").append(tr);
   
 };
 
